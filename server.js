ab133ba30a6ff3b3bfca80d69e1deaf818518046
Rafaelnascimento <rkrafael.nascimento@gmail.com>
2018-12-24 12:10:58 -0200

    First example with express and config package.json

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-const http = require("http")
+const express = require("express")
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.end(`<html>
-                <head>
-                     <meta charset="UTF-8"/>
-                </head>
-                <body>
-                     <h1> NodeJs </h1>
-                </body> 
-             </html>`
-    );
-})
 
-server.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server listening on ${port}`)
 })
+
+app.get('/', (req, res) => {
+    res.end(`<html>
+            <head>
+                <meta charset="UTF-8"/>
+            </head>
+            <body>
+                <h1> NodeJs </h1>
+            </body> 
+            </html>`
+    )
+})
+
+
+app.get('/list', (req, res) => {
+    res.end(`<html>
+            <head>
+                <meta charset="UTF-8"/>
+            </head>
+            <body>
+                <h1> List NodeJs </h1>
+            </body> 
+            </html>`
+    )
+})
+
